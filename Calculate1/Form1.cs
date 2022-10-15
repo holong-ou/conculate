d422b403fe365f7223947c8bc1e954329c13fed7
holong-ou <2051012057long@ou.edu.vn>
2022-10-15 16:38:30 +0700

    Upload

--- Calculate1/Form1.cs
+++ Calculate1/Form1.cs
@@ -16,5 +16,17 @@ namespace Calculate1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            label3.Text = Convert.ToString(a * b);
+        }
     }
 }
